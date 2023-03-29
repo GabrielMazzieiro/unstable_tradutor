293603795f8f0baff4077a9fdf829196d3a268fc
GabrielMazzieiro <gabrielpm94@gmail.com>
2023-03-28 21:12:23 -0300

    chore: add minor changes

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -10,7 +10,7 @@ const TopBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav nav-underline me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link" aria-current="page" href="/">Home</a>
               </li>
               {/* <li className="nav-item">
                 <a className="nav-link" href="#">Link</a>
