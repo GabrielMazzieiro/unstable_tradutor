3f2c9479cd478093dd9bdd48502612fb0995014b
GabrielMazzieiro <gabrielpm94@gmail.com>
2023-04-18 22:03:25 -0300

    feat: add all unstable unicorns cards

--- src/pages/UU.tsx
+++ src/pages/UU.tsx
@@ -1,11 +1,24 @@
 import { TopBar } from "../components"
+import ShowCards from "../components/ShowCards"
+import uuCards from "../db/uu"
 
 const UUPage = () => {
+
+    const title_list = [
+    'Maldição',
+    'Instantânea',
+    'Unicórnio Básico',
+    'Encantamento',
+    'Unicórnio Supremo',
+    'Mágica',
+    'Unicórnio Bebê',
+    'Unicórnio Mágico']
 
     return (
      <main>
         <TopBar/>
         <p>UNSTABLE UNICORNS PAGE</p>
+        <ShowCards cards={uuCards} titles={title_list}/>
 
 
      </main>
