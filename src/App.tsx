a9c85aa6afca3c17180dbf8bdbff284726fd0416
GabrielMazzieiro <gabrielpm94@gmail.com>
2023-04-28 12:41:41 -0300

    feat: add generic routes for each game

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Home, HLDPage, HTSPage, UUPage, LLUPage, WPPage, TTKOPage } from './pages'
+import { Home, LLUPage, WPPage, TTKOPage } from './pages'
+import GamePage from './pages/Games'
 
 
 
@@ -10,9 +11,9 @@ function App() {
   return (
     <Routes>
       <Route key="home" path="/" element={<Home/>}/>
-      <Route key="hld" path="/hld" element={<HLDPage/>}/>
-      <Route key="hts" path="/hts" element={<HTSPage/>}/>
-      <Route key="uu" path="/uu" element={<UUPage/>}/>
+      <Route key="hld" path="/hld" element={<GamePage game={"hld"}/>}/>
+      <Route key="hts" path="/hts" element={<GamePage game={"hts"}/>}/>
+      <Route key="uu" path="/uu" element={<GamePage game={"uu"}/>}/>
       <Route key="hlluome" path="/llu" element={<LLUPage/>}/>
       <Route key="wp" path="/wp" element={<WPPage/>}/>
       <Route key="ttko" path="/ttko" element={<TTKOPage/>}/>
