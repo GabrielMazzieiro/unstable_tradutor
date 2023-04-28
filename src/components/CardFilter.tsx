b410f4d64520e93dfda5449ab14b18f3d4030e53
GabrielMazzieiro <gabrielpm94@gmail.com>
2023-04-28 12:40:36 -0300

    feat: add search filter logic

--- src/components/CardFilter.tsx
+++ src/components/CardFilter.tsx
@@ -2,12 +2,17 @@ import React, { Dispatch, SetStateAction } from "react";
 import { ICardData } from "./ShowCards.types";
 
 interface ICardFilter{
-    cards: ICardData[]
+    base_editions: string[]
+    expansions: string[]
     setEditions: Dispatch<SetStateAction<string[]>>;
+    search: string
+    setSearch: Dispatch<SetStateAction<string>>;
+    cards: ICardData[]
+    setFilteredResults: Dispatch<SetStateAction<ICardData[]>>
 }
 
 
-const CardFilter = ({cards, setEditions}: ICardFilter) => {
+const CardFilter = ({base_editions, expansions, setEditions, search, setSearch, cards, setFilteredResults}: ICardFilter) => {
 
     
     function handleCheckbox(event: { target: { value: string; checked: boolean } }){
@@ -26,104 +31,52 @@ const CardFilter = ({cards, setEditions}: ICardFilter) => {
         }
     }
 
-
+    function handleSearchbox(event: { target: { value: React.SetStateAction<string>; }; }){
+        setSearch(event.target.value)
+        if (search !== ''){
+        const filteredData = cards.filter((item) => {
+            return Object.values(item).join('').toLowerCase().includes(search.toLowerCase())
+        })
+        setFilteredResults(filteredData)}
+    }
     
-
     return (
         <>
         <form className="gamefilter d-flex" role="search">
             <div className="row gamefilter-row gy-3">
                 <p className="filter-title">Jogos Básicos</p>
                 <div className="filter-edition">
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="Base Game" onChange={handleCheckbox} id="Jogo Clássico"/>
-                        <label className="form-check-label" htmlFor="Jogo Clássico">
-                            Base Game
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="NSFW Base Deck" onChange={handleCheckbox} id="NSFW Base Deck"/>
-                        <label className="form-check-label" htmlFor="NSFW Base Deck">
-                            NSFW
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="Control Base Deck" onChange={handleCheckbox} id="Control Base Deck"/>
-                        <label className="form-check-label" htmlFor="Control Base Deck">
-                            Control
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="Chaos Base Deck" onChange={handleCheckbox} id="Chaos Base Deck"/>
-                        <label className="form-check-label" htmlFor="Chaos Base Deck">
-                            Chaos
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="Travel" onChange={handleCheckbox} id="Travel" disabled/>
-                        <label className="form-check-label" htmlFor="Travel">
-                            Travel
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="For Kids" onChange={handleCheckbox} id="For Kids" disabled/>
-                        <label className="form-check-label" htmlFor="For Kids">
-                            For Kids
-                        </label>
-                    </div>
+                    {base_editions.map(edition => {
+                        return (
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value={edition} onChange={handleCheckbox} id={edition}/>
+                                <label className="form-check-label" htmlFor={edition}>
+                                    {edition}
+                                </label>
+                            </div>
+                        )
+                    })}        
                 </div>  
             </div>
             <div className="row gamefilter-row gy-3">
                 <p className="filter-title">Expansões</p>
                 <div className="filter-edition">
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="NSFWexpansion" disabled/>
-                        <label className="form-check-label" htmlFor="NSFWexpansion">
-                            NSFW
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="Apocalipse Arco-Iris" disabled/>
-                        <label className="form-check-label" htmlFor="Apocalipse Arco-Iris">
-                            Apocalipse Arco-Iris
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="Dragões" disabled/>
-                        <label className="form-check-label" htmlFor="Dragões">
-                            Dragões
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="Unicorns of Legend" disabled/>
-                        <label className="form-check-label" htmlFor="Unicorns of Legend">
-                            Unicorns of Legend
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="Adventures" disabled/>
-                        <label className="form-check-label" htmlFor="Adventures">
-                            Adventures
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="Christmas" disabled/>
-                        <label className="form-check-label" htmlFor="Christmas">
-                            Christmas
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="Nightmares" disabled/>
-                        <label className="form-check-label" htmlFor="Nightmares">
-                            Nightmares
-                        </label>
-                    </div>
+                {expansions.map(edition => {
+                        return (
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value={edition} onChange={handleCheckbox} id={edition}/>
+                                <label className="form-check-label" htmlFor={edition}>
+                                    {edition}
+                                </label>
+                            </div>
+                        )
+                    })}     
                 </div>
             </div>
         </form>
 
         <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Nome da Carta" aria-label="Search"/>
+                <input className="form-control me-2" type="search" onChange={handleSearchbox} placeholder="Nome ou Palavra Chave" aria-label="Search"/>
                 <button className="btn btn-outline-success" type="button">Procurar</button>
         </form>
         </>
--- src/components/ShowCards.tsx
+++ src/components/ShowCards.tsx
@@ -7,10 +7,12 @@ import type { ICardData } from './ShowCards.types'
 interface ShowCardProps{
     cards: ICardData[];
     titles: string[];
-    editions: string[]
+    editions: string[];
+    search: string;
+    filteredResults: ICardData[];
 }
 
-const ShowCards = ({cards, titles, editions}:ShowCardProps) => {
+const ShowCards = ({cards, titles, editions, search, filteredResults}:ShowCardProps) => {
 
     const [data, setData] = useState<ICardData>()
 
@@ -19,24 +21,13 @@ const ShowCards = ({cards, titles, editions}:ShowCardProps) => {
         {titles.map(title => {
             return(
                 <>
-                <div><h1 className="title"> {title} </h1></div>
-
-                <div className="row gy-3 card-row">
-                    {cards.map(item => {
-                        if (item.card_type_br == title){
-                            if (editions.length === 0){
-                                return (
-                                    <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
-                                        <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
-                                            <img src={getCardImage(item.card_id, item.game)} className="card-img-top" alt={item.card_name}/>
-                                        </button>
-                                    </div>
-                                )
-                            } else {
-                                return(
-                                    <>
-                                    {editions.map(edition => {
-                                        if (item.card_edition == edition)
+                    <div><h1 className="title"> {title} </h1></div>
+                    
+                    <div className="row gy-3 card-row">
+                        {search.length > 1 ? (
+                            filteredResults.map((item) => {
+                                if (item.card_type_br == title){
+                                    if (editions.length === 0){
                                         return (
                                             <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
                                                 <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
@@ -44,13 +35,84 @@ const ShowCards = ({cards, titles, editions}:ShowCardProps) => {
                                                 </button>
                                             </div>
                                         )
-                                    })}
-                                    </>
-                                )
+                                    } else {
+                                        return(
+                                            <>
+                                            {editions.map(edition => {
+                                                if (item.card_edition == edition)
+                                                return (
+                                                    <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
+                                                        <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
+                                                            <img src={getCardImage(item.card_id, item.game)} className="card-img-top" alt={item.card_name}/>
+                                                        </button>
+                                                    </div>
+                                                )
+                                            })}
+                                            </>
+                                        )
+                                    }
+                                }
+                            })
+                            
+                        ):(
+                            cards.map((item) => {
+                                if (item.card_type_br == title){
+                                    if (editions.length === 0){
+                                        return (
+                                            <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
+                                                <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
+                                                    <img src={getCardImage(item.card_id, item.game)} className="card-img-top" alt={item.card_name}/>
+                                                </button>
+                                            </div>
+                                        )
+                                    } else {
+                                        return(
+                                            <>
+                                            {editions.map(edition => {
+                                                if (item.card_edition == edition)
+                                                return (
+                                                    <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
+                                                        <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
+                                                            <img src={getCardImage(item.card_id, item.game)} className="card-img-top" alt={item.card_name}/>
+                                                        </button>
+                                                    </div>
+                                                )
+                                            })}
+                                            </>
+                                        )
+                                    }
+                            }})
+                        )}
+
+                        {/* {cards.map(item => {
+                            if (item.card_type_br == title){
+                                if (editions.length === 0){
+                                    return (
+                                        <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
+                                            <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
+                                                <img src={getCardImage(item.card_id, item.game)} className="card-img-top" alt={item.card_name}/>
+                                            </button>
+                                        </div>
+                                    )
+                                } else {
+                                    return(
+                                        <>
+                                        {editions.map(edition => {
+                                            if (item.card_edition == edition)
+                                            return (
+                                                <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
+                                                    <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
+                                                        <img src={getCardImage(item.card_id, item.game)} className="card-img-top" alt={item.card_name}/>
+                                                    </button>
+                                                </div>
+                                            )
+                                        })}
+                                        </>
+                                    )
+                                }
                             }
-                        }
-                    })}
-                </div>
+                        })}   */}
+                    </div>
                 </>
             )
         })}
