92b02963483d32f684e14d09b33c89b9dbe767f2
GabrielMazzieiro <gabrielpm94@gmail.com>
2023-05-05 17:08:00 -0300

    refactor: cleaning some code

--- src/components/ShowCards.tsx
+++ src/components/ShowCards.tsx
@@ -15,7 +15,7 @@ interface ShowCardProps{
 const ShowCards = ({cards, titles, editions, search, filteredResults}:ShowCardProps) => {
 
     const [data, setData] = useState<ICardData>()
-
+    
     return(
         <>
         {titles.map(title => {
@@ -83,35 +83,6 @@ const ShowCards = ({cards, titles, editions, search, filteredResults}:ShowCardPr
                                     }
                             }})
                         )}
-
-                        {/* {cards.map(item => {
-                            if (item.card_type_br == title){
-                                if (editions.length === 0){
-                                    return (
-                                        <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
-                                            <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
-                                                <img src={getCardImage(item.card_id, item.game)} className="card-img-top" alt={item.card_name}/>
-                                            </button>
-                                        </div>
-                                    )
-                                } else {
-                                    return(
-                                        <>
-                                        {editions.map(edition => {
-                                            if (item.card_edition == edition)
-                                            return (
-                                                <div  className='col col-4 col-sm-3 col-lg-2 col-xxl-1 card-back'>
-                                                    <button className='card-button' onClick={() => setData(item)} data-bs-toggle="modal" data-bs-target='#card'>
-                                                        <img src={getCardImage(item.card_id, item.game)} className="card-img-top" alt={item.card_name}/>
-                                                    </button>
-                                                </div>
-                                            )
-                                        })}
-                                        </>
-                                    )
-                                }
-                            }
-                        })}   */}
                     </div>
                 </>
             )
